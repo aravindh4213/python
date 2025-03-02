2f4b70cde1f9c10f498cedceaff8292a24f6abf9
aravindh raja <144410301+aravindh4213@users.noreply.github.com>
2025-03-02 09:23:02 +0530

    python
    practice programing

---
+++ AreaCalc.py
@@ -0,0 +1,17 @@
+import math
+def circle(radius):
+    """
+    to Calculate the area of circle
+    radius-> Enter the radius of the circle
+    """
+    area =math.pi*radius**2
+    return area
+
+def rectangle(length,width):
+    """"
+    to Calculate the area of the rectangle
+    length-> Enter the length of the rectangle
+    width-> Enter the width of the rectangle
+    """
+    area=length*width
+    return area
